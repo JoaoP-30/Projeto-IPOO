32753fb36eb47f11e25c457de30455c48d46c4e6
Joao Pedro <joaopedrofernandes01@hotmail.com>
2025-10-31 02:38:02 -0300

    Estrutura inicial do projeto 'Saltos e Segredos' com classes base criadas (sem implementação)

---
+++ Saltos e Segredos/Tela_Inicial.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Tela_Inicial here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Tela_Inicial extends World
+{
+
+    /**
+     * Constructor for objects of class Tela_Inicial.
+     * 
+     */
+    public Tela_Inicial()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+    }
+}
