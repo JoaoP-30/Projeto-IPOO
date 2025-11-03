64d34f160a1ca8add14ba90d5279066128eb1681
Joao Pedro <joaopedrofernandes01@hotmail.com>
2025-11-03 20:45:44 -0300

    adiciona classe Jogador com movimentação, animação e controle de vida

--- Saltos e Segredos/Jogador.java
+++ Saltos e Segredos/Jogador.java
@@ -1,19 +1,104 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Jogador here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
+ * Representa o jogador principal no jogo.
+ * Esta classe controla o movimento, as animações e estado (como vida)
+ * do personagem controlado pelo usuário.
+ * @author Joao Fernandes 
+ * @version 1.0
  */
+
 public class Jogador extends Actor
 {
+    //Array de imagens para a animação do jogador movendo-se para a direita.
+    private GreenfootImage[] animacaoD;
+    //Array de imagens para a animação do jogador movendo-se para a esquerda.
+    private GreenfootImage[] animacaoE;
+    //Contador para controlar o frame atual da animação.
+    private int frame;
+    //Contador de vida do jogador
+    private static int vida;
+    //Velocidade de movimento do jogador
+    private int velMovimento;
+
     /**
-     * Act - do whatever the Jogador wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
+     * Construtor da classe Jogador.
+     * Inicializa os arrays de animação carregando as imagens.
+     * Define os valores iniciais para o frame de animação, a vida e a velocidade de movimento. 
      */
+    
+    public Jogador(){
+        animacaoD = new GreenfootImage[4];
+        animacaoE = new GreenfootImage[4];
+
+        for (int i = 0; i < animacaoD.length; i++){
+            animacaoD[i] = new GreenfootImage("/jogador/run" + (i + 1) + "r.png");
+            animacaoE[i] = new GreenfootImage("/jogador/run" + (i + 1) + "l.png");
+        }
+
+        frame = 0;
+
+        vida = 2;
+
+        velMovimento = 5;
+    }
+
+    /**
+     * Método chamado automaticamente pelo Greenfoot a cada ciclo de execução.
+     * Método principal de atuação (loop) da classe Jogador.
+     * Ele chama o método de movimento.
+     */
+    
     public void act()
     {
-        // Add your action code here.
+        movimento();
+    }
+
+    /**
+     * Verifica a entrada do teclado (teclas "right" e "left") e
+     * chama os métodos de movimento correspondentes.
+     */
+    
+    private void movimento(){
+        if(Greenfoot.isKeyDown("right")){
+            moverDireita();
+        }
+        if(Greenfoot.isKeyDown("left")){
+            moverEsquerda();
+        }
+    }
+
+    /**
+     * Executa o movimento do jogador para a direita.
+     * Move o ator 'velMovimento' pixels e atualiza o sprite (imagem)
+     * para o próximo quadro da animação 'animacaoD'.
+     */
+    
+    private void moverDireita(){
+        move(velMovimento);
+
+        if(frame % 4 == 0){
+            frame = 0;
+        }
+
+        setImage(animacaoD[frame]);
+        frame++;
+    }
+
+    /**
+     * Executa o movimento do jogador para a esquerda.
+     * Move o ator '-velMovimento' pixels (para trás) e atualiza o sprite (imagem)
+     * para o próximo quadro da animação 'animacaoE'.
+     */
+    
+    private void moverEsquerda(){
+        move(-velMovimento);
+
+        if(frame % 4 == 0){
+            frame = 0;
+        }
+
+        setImage(animacaoE[frame]);
+        frame++;
     }
 }
